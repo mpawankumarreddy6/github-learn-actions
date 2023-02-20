4a459bd554a38f3e628e6e092e2872e2d99e7141
mpawankumarreddy6 <72983453+mpawankumarreddy6@users.noreply.github.com>
2023-02-20 19:00:31 +0530

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,13 @@
+﻿param(    [string]$RelativePath = "D:\New folder") 
+$jsonfiles = Get-ChildItem -Path $RelativePath -Filter *.json -Recurse
+foreach($file in $jsonfiles)
+{ $newContent = Invoke-WebRequest $file.FullName | ConvertFrom-Json    
+     $oldBuildNumber = $newContent.version    
+     $MajorVersion = $oldBuildNumber.split('.')[0]    
+     $MinorVersion = $oldBuildNumber.split('.')[1]    
+     $PatchVersion = $oldBuildNumber.split('.')[2]    
+     $PatchVersionNew = [int]$PatchVersion + 1     
+     $newBuildNumber = $MajorVersion + '.' + $MinorVersion + '.' + [string]$PatchVersionNew    
+     $newContent.version = $newBuildNumber     
+     ConvertTo-Json -InputObject $newContent -Depth 10 | set-content $file.FullName    
+}

c5dcdb1a5ab31fbb24bc8cf63eaf45ce0c35952a
mpawankumarreddy6 <72983453+mpawankumarreddy6@users.noreply.github.com>
2023-03-14 18:00:34 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,14 +1,40 @@
-$jsonfiles = Get-ChildItem -Path ./github-learn-actions -Filter *.json -Recurse
+param(
+    [string]$RelativePath = "./src"
+)
+
+git diff --name-only
+
+echo "outside"
+
+$jsonfiles = Get-ChildItem -Path $RelativePath -Filter manifest.json -Recurse
 foreach($file in $jsonfiles)
-{ 
-     write-host $file.FullName
-     $newContent = Invoke-WebRequest $file.FullName | ConvertFrom-Json    
-     $oldBuildNumber = $newContent.version    
-     $MajorVersion = $oldBuildNumber.split('.')[0]    
-     $MinorVersion = $oldBuildNumber.split('.')[1]    
-     $PatchVersion = $oldBuildNumber.split('.')[2]    
-     $PatchVersionNew = [int]$PatchVersion + 1     
-     $newBuildNumber = $MajorVersion + '.' + $MinorVersion + '.' + [string]$PatchVersionNew    
-     $newContent.version = $newBuildNumber     
-     ConvertTo-Json -InputObject $newContent -Depth 10 | set-content $file.FullName    
+{
+    echo "inside"
+    
+    # Read the file contents as a JSON object
+    $json = Get-Content $file -Raw | ConvertFrom-Json
+    
+    # Extract the current version number and split it into its components
+    $versionParts = $json.version -split '\.'
+
+    # Increment the last component of the version number
+    $versionParts[-1] = [int]$versionParts[-1] + 1
+
+    # Combine the version components into a string
+    $newVersion = $versionParts -join '.'
+    
+    echo $newVersion
+
+    # Update the version number in the JSON object
+    $json.version = $newVersion
+    
+    echo "New " + $json.version
+
+    # Save the updated JSON object back to the file
+    $json | ConvertTo-Json -Depth 100 | Set-Content $file
+    
+    ConvertTo-Json -InputObject $json -Depth 100 | Set-Content $file
+    
+     $json1 = Get-Content $file -Raw | ConvertFrom-Json
+    echo $json1
 }

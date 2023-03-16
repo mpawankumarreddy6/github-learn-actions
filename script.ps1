e774556a6bf01e64f45707ff17247f39566a3f02
mpawankumarreddy6 <72983453+mpawankumarreddy6@users.noreply.github.com>
2023-03-16 19:03:26 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,40 +1,11 @@
-param(
-    [string]$RelativePath = "./src"
-)
-
-git diff --name-only
-
-echo "outside"
-
-$jsonfiles = Get-ChildItem -Path $RelativePath -Filter manifest.json -Recurse
-foreach($file in $jsonfiles)
+$repoPath = "$env:GITHUB_WORKSPACE"
+$filePath = "$repoPath\manifest.json"
+$content = Get-Content $filePath
+if ($content -ne $null)
 {
-    echo "inside"
-    
-    # Read the file contents as a JSON object
-    $json = Get-Content $file -Raw | ConvertFrom-Json
-    
-    # Extract the current version number and split it into its components
-    $versionParts = $json.version -split '\.'
-
-    # Increment the last component of the version number
-    $versionParts[-1] = [int]$versionParts[-1] + 1
-
-    # Combine the version components into a string
-    $newVersion = $versionParts -join '.'
-    
-    echo $newVersion
-
-    # Update the version number in the JSON object
-    $json.version = $newVersion
-    
-    echo "New " + $json.version
-
-    # Save the updated JSON object back to the file
-    $json | ConvertTo-Json -Depth 100 | Set-Content $file
-    
-    ConvertTo-Json -InputObject $json -Depth 100 | Set-Content $file
-    
-     $json1 = Get-Content $file -Raw | ConvertFrom-Json
-    echo $json1
+        echo "inside"
+} 
+else 
+{
+        echo "failed"
 }

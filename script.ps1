a552c5296d62924f24155d2b66921768515c478d
mpawankumarreddy6 <72983453+mpawankumarreddy6@users.noreply.github.com>
2023-03-16 19:10:25 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -3,7 +3,28 @@ $filePath = "$repoPath\manifest.json"
 $content = Get-Content $filePath
 if ($content -ne $null)
 {
-        echo "inside"
+    echo "inside"
+
+    $json = Get-Content $file -Raw | ConvertFrom-Json
+    
+    # Extract the current version number and split it into its components
+    $versionParts = $json.version -split '\.'
+
+    # Increment the last component of the version number
+    $versionParts[-1] = [int]$versionParts[-1] + 1
+
+    # Combine the version components into a string
+    $newVersion = $versionParts -join '.'
+    
+    echo $newVersion
+
+    # Update the version number in the JSON object
+    $json.version = $newVersion
+    
+    echo "New " + $json.version
+
+    # Save the updated JSON object back to the file
+    $json | ConvertTo-Json -Depth 100 | Set-Content $filePath
 } 
 else 
 {
